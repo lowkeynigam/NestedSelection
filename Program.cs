d908079fd7eae5eebfcaf6592f5bc2852b6ce5a1
Rishabh Nigam <fire2407doesminecraft@gmail.com>
2021-03-18 18:23:07 +1100

    Real Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username1 = "HalcyonicFlame";
+            string password1 = "leaguepassword";
+
+                 Console.WriteLine("Please enter your Username and Password: ");
+                    string input1 = Console.ReadLine();
+                    string input2 = Console.ReadLine();
+
+                     if (input1 == username1 && input2 == password1) 
+                     {
+                         Console.WriteLine("Login successful!");
+                     }
+                     else 
+                     {
+                        Console.WriteLine("Login unsuccessful.");
+                     }
+                        
+                    
+                 
+
+
+
+                
+            
+        }
+    }
+}
